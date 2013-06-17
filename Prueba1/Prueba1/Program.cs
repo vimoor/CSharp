52775f71ebba66d80c0a1dc661220dc33506685f
Vicent Morera Orchilles <vimoor1@inf.upv.es>
2013-06-17 15:57:12 +0200

    Gestión de arrays incorporada
    He incorporado un tostring para arrays de elementos del tipo integer,
    una función que comprueba si un elemento és primo o no, y otra que
    rellena un array con los X primeros números primos.

--- Prueba1/Prueba1/Program.cs
+++ Prueba1/Prueba1/Program.cs
@@ -27,6 +27,74 @@ namespace ConsoleApplication1
             System.Console.WriteLine(texto);
         }
 
+        /**
+         * Función ArrayIntToString()
+         * recibe como argumento un array de enteros y lo devuelve como String
+         **/
+        static String ArrayIntToString(int [] array)
+        {
+            String salida;
+            if(array.Length >= 1)
+            {
+                salida = ""+array[0];
+                for (int i = 1; i < array.Length; i++)
+                {
+                    salida += ", "+array[i];
+                }
+            }
+            else
+            {
+                salida = "El array introducido está vacío";
+            }
+            return salida; 
+         }
+        
+        /**
+         *Función primos(int x)
+         *esta función devuelve los x primeros números primos y los devuelve en forma de array.
+         * 
+         **/
+        static int [] primos(int longitud)
+        {
+            int cuenta = 1;
+            int[] salida = new int[longitud];
+            for (int i = 0; i < longitud; i++)
+            {
+                //echo("contador " + i);
+                while(!primo(cuenta))
+                {
+                    //echo("cuenta " + cuenta);
+                    cuenta++;   
+                }
+                salida[i] = cuenta;
+                cuenta++;
+            }
+            return salida;
+        }
+
+        /**
+         * Función primo(int x)
+         * esta función devuelve true si x es primo, o false si no lo és
+         * en caso de ser 1, devuelve directamente true.
+         * */
+        static bool primo(int numero)
+        {
+            if (numero == 1) return true;
+            int divisibles = 0;
+            for (int i = numero; i > 0; i--)
+            {
+                if (numero % i == 0)
+                {
+                    divisibles++;
+                }
+            }
+            if (divisibles == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
         static void Main(string[] args)
         {
             //escritura por pantalla.
@@ -37,6 +105,12 @@ namespace ConsoleApplication1
             echo("Hola, "+nombre);
             int edad = Convert.ToInt32(args[0]);
             echo("Tiene "+edad+" años, verdad?");
+            int[] prim;
+            int longitud = 10;
+            prim = new int[longitud];
+            prim = primos(longitud);
+            echo(ArrayIntToString(prim));
+            //echo(""+primo(27));
             final();
         }
     }
